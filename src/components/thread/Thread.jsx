2117e9c6154a4187042e7d692903fd9470c28fe9
Riska <rchika532@gmail.com>
2024-07-25 23:13:03 +0700

    membuat komponen thread

---
+++ src/components/thread/Thread.jsx
@@ -0,0 +1,14 @@
+import "./Thread.css";
+
+function Thread() {
+  return (
+    <div className="cards">
+      <h1>name</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, quam!
+      </p>
+    </div>
+  );
+}
+
+export default Thread;
